ceaf66e0439aa0ff00cd7ef6eb0f1ba48101db19
TheJeef <el.the.boss@gmail.com>
2017-10-17 20:35:57 +0000

    Updated show template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
-      render :new
+      render 'new'
     end
   end
   

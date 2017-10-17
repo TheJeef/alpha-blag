9384df83cfde4fb508ed6746fd72432f4d0228fd
TheJeef <el.the.boss@gmail.com>
2017-10-17 21:43:55 +0000

    removed redundancies and prepped for production deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
   
   def index
     @all_artilces = Article.all
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+
   end
   
   def create
@@ -23,11 +24,10 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -37,7 +37,6 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article #{@article.id} was deleted"
     redirect_to articles_path
@@ -47,6 +46,10 @@ class ArticlesController < ApplicationController
   
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end

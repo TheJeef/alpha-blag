43089e9f88bc165cab1698887d8b58cc4b25602e
TheJeef <el.the.boss@gmail.com>
2017-12-03 03:51:06 +0000

    update all views with categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,5 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
+  validates :category_ids, presence: { message: "Please select at least one category" }
 end
